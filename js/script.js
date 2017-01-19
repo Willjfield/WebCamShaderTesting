513f1fcede9942ac596b537ab79fbd81365d9e5c
Willjfield <willjfield@gmail.com>
2017-01-18 21:44:31 -0500

    Video distorted teapot

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ if (navigator.getUserMedia) {
 }
 
 // three.js cube drawing
-
+var teapot, material;
 function threeRender(video) {
 
 var scene = new THREE.Scene();
@@ -58,28 +58,52 @@ var texture = new THREE.VideoTexture( video );
 texture.minFilter = THREE.LinearFilter;
 texture.magFilter = THREE.LinearFilter;
 texture.format = THREE.RGBFormat;
+texture.needsUpdate = true;
 //texture.repeat.set( 1, 1 ); 
 
-var geometry = new THREE.PlaneGeometry(16,9,64,36);
-//var material = new THREE.MeshLambertMaterial( { map: texture, shading: THREE.FlatShading } );
-var material = new THREE.ShaderMaterial( {
+//var geometry = new THREE.PlaneGeometry(16,9,64,36);
+//var material = new THREE.MeshLambertMaterial( { color: 0xffffff, side: THREE.DoubleSide,  shading: THREE.FlatShading } );
+material = new THREE.ShaderMaterial( {
         uniforms: {
-            time: { type: "f", value: 1.0 },
+            time: { type: "f", value: 1 },
             webcam: { type: "t", value: texture}
         },
         vertexShader: document.getElementById( 'vertexShader' ).textContent,
         fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
         side: THREE.DoubleSide  
     } );
-geometry.computeFaceNormals();
-geometry.computeVertexNormals();
-//material.side = THREE.DoubleSide;
-//material.map.minFilter = THREE.LinearFilter;
-var plane = new THREE.Mesh( geometry, material );
-//plane.rotation.y = Math.PI/4;
-scene.add( plane );
-camera.position.z = 10;
-//camera.position.y = 1;
+
+// //material.side = THREE.DoubleSide;
+// //material.map.minFilter = THREE.LinearFilter;
+// var plane = new THREE.Mesh( geometry, material );
+// //plane.rotation.y = Math.PI/4;
+// scene.add( plane );
+
+var loader = new THREE.OBJLoader( );
+        loader.load( 'teapot.obj', function ( object ) {
+          console.log(object);
+          object.traverse( function ( child ) {
+
+            if ( child instanceof THREE.Mesh ) {
+              child.material = material;
+              child.material.map = texture;
+              child.geometry.computeFaceNormals();
+              child.geometry.computeVertexNormals();
+              child.material.needsUpdate = true;
+              child.geometry.uvsNeedUpdate = true;
+              child.geometry.buffersNeedUpdate = true;
+            }
+
+          } );
+          object.children[0].material.needsUpdate = true;
+          teapot = object;
+          teapot.position.z = -200;
+          scene.add( teapot );
+
+        });
+camera.position.z = 15;
+camera.position.y = 25;
+camera.position.x = 25;
 
 var light = new THREE.AmbientLight( 'rgb(255,255,255)' ); // soft white light
 scene.add( light );
@@ -100,16 +124,24 @@ spotLight.shadow.camera.far = 4000;
 spotLight.shadow.camera.fov = 30;
 scene.add( spotLight );
 
-//render the scene
 
+
+//render the scene
+var counter=0;
 function render() {
+  counter++;
   requestAnimationFrame(render);
-  plane.geometry.computeFaceNormals();
-  plane.geometry.computeVertexNormals();
-  // sphere.rotation.x += 0.001;
-  //plane.rotation.y += 0.001;
+  // plane.geometry.computeFaceNormals();
+  // plane.geometry.computeVertexNormals();
+  // // sphere.rotation.x += 0.001;
+  if(teapot){
+    teapot.rotation.y += 0.005;
+    teapot.children[0].material.uniforms.time=counter;
+    teapot.children[1].material.uniforms.time=counter;
+  }
+  
   texture.needsUpdate = true;
-  plane.geometry.verticesNeedUpdate = true;
+  // plane.geometry.verticesNeedUpdate = true;
   renderer.render(scene, camera);
 }
 
